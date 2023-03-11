6db7a5261e4029e7612157ea1f8395e6de63a62a
OGEmanuel <emanuolumide@mail.com>
2023-03-11 09:41:29 +0100

    iframe adjusted

--- components/video.jsx
+++ components/video.jsx
@@ -4,14 +4,14 @@ const Video = () => {
   return (
     <section>
       <h4 className="h4">WATCH THIS VIDEO TO LEARN MORE ABOUT OUR PRODUCT</h4>
-      <div className="w-full mb-10">
+      <div className="px-20 mb-10">
         <iframe
           src="https://youtube.com/embed/mzZt6faN8wo"
           allow="autoplay; encrypted-media"
           allowFullScreen
           title="video"
           frameborder="0"
-          className="w-[87%] h-[35.0625rem] mx-auto"
+          className="h-[35.0625rem] w-full"
         />
       </div>
       <CTAMain />

c7f741ff0c675ffc2530641aabc4e7c1a9b97dd7
OGEmanuel <emanuolumide@mail.com>
2023-03-11 09:10:50 +0100

    how complete

--- components/how.jsx
+++ components/how.jsx
@@ -2,31 +2,33 @@ const How = () => {
   return (
     <section>
       <h4 className="h4">HOW IT WORKS</h4>
-      <div className="px-20 font-mont font-bold text-center">
-        <div>
-          <p>Create an app</p>
-          <p>
+      <div className="px-20 font-mont font-bold text-center grid-custom">
+        <div className="bg-how-bg-1 how-container">
+          <p className="how-main-p text-benefits-main-2">Create an app</p>
+          <p className="how-sub-p text-how-sub-1">
             Create an app on the dashboard. During creation, select the product
             type; KYC in this case
           </p>
         </div>
-        <div>
-          <p>integrate app link</p>
-          <p>
+        <div className="bg-benefits-main-3 how-container">
+          <p className="how-main-p text-white">integrate app link</p>
+          <p className="how-sub-p text-card-p">
             Copy unique app link and send to customers or integrate on the
             platform for customers to click
           </p>
         </div>
-        <div>
-          <p>Get authentication from customers</p>
-          <p>
+        <div className="bg-how-bg-3 how-container">
+          <p className="how-main-p text-benefits-main-3">
+            Get authentication from customers
+          </p>
+          <p className="how-sub-p text-header-body">
             Customers keys in correct bank details (for any bank at all in
             Nigeria)
           </p>
         </div>
-        <div>
-          <p>confirm query</p>
-          <p>
+        <div className="how-container shadow-2xl">
+          <p className="how-main-p text-benefits-main-3">confirm query</p>
+          <p className="how-sub-p text-header-body">
             If successful, you will be notified by email about newly added
             customers
           </p>
--- components/partners.jsx
+++ components/partners.jsx
@@ -6,11 +6,11 @@ import tomx from '../public/images/tomxcredit.png';
 
 const Partners = () => {
   return (
-    <section className="flex-items-center px-24 gap-8 mb-20">
+    <section className="flex-items-center flex-wrap px-24 gap-8 mb-20">
       <p className="font-bold text-[1.25rem] font-mont text-partners-color">
         Trusted by
       </p>
-      <div className="flex-items-center gap-20">
+      <div className="flex-items-center flex-wrap gap-20">
         <Image src={daxlinks} alt="daxlinks logo" />
         <Image src={tomx} alt="tomxcredit logo" />
         <Image src={credley} alt="credley logo" />
